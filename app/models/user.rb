430674d30936dec78d4f8a01b0b6bed5fbe3fdc8
Matsu-pt <rx_77_3@icloud.com>
2021-04-08 17:51:03 +0900

    deviceの導入とUserモデルへのバリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with:/\A(?:\p{Hiragana}|\p{Katakana}|[一-龠々])+\z/}
+      validates :first_name
+      validates :family_name
+    end
+    with_options format: { with:/[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/}
+      validates :first_name_ruby
+      validates :family_name_ruby
+    end
+    validates :birth_day
+  end
+
+  validates :encrypted_password, format: { with:/\A[a-z0-9]+\z/i}
+end
