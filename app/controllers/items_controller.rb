487e8a08985115fa70c963cc0455e145761feee3
Matsu-pt <rx_77_3@icloud.com>
2021-04-16 16:43:55 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :create]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :much_current_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -24,12 +25,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    much_current_user
+    
   end
 
   def update
-    much_current_user
-    @item.update(item_params)
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
